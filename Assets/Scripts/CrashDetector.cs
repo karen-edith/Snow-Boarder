3b22d85524136d8325830e7b2c195750ea26fac6
Karen Garcia <karengarcia@Karens-Air.lan>
2022-02-19 10:32:14 -0800

    Snow Boarder

---
+++ Assets/Scripts/CrashDetector.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrashDetector : MonoBehaviour
+{
+   void OnTriggerEnter2D(Collider2D other) 
+  {
+      if(other.tag == "Ground") 
+      {
+          Debug.Log("You Have to restart!");
+      }
+      
+  }
+}
