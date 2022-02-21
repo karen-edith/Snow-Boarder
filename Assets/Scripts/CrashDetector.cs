8b9748ae3cafce44d74885c1bb159f45f0d000e6
Karen Garcia <karengarcia@Karens-Air.lan>
2022-02-21 11:42:41 -0800

    restart game on crash and finish and add delay to scene change

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,15 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
+//using System.Collections;
+//using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float CrashSceneDelay = 2f;
    void OnTriggerEnter2D(Collider2D other) 
   {
       if(other.tag == "Ground") 
       {
-          Debug.Log("You Have to restart!");
+          Invoke("ReloadScene", CrashSceneDelay);
       }
       
   }
+
+  void ReloadScene () 
+  {
+      SceneManager.LoadScene(0);
+  }
+
 }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -1,15 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
+//we really don't use these namespaces so we can comment them out
+//they are by default placed here
+//using System.Collections;
+//using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+  [SerializeField] float SceneDelay = 2f;
   void OnTriggerEnter2D(Collider2D other) 
   {
       if(other.tag == "Player") 
       {
-          Debug.Log("You Finished!");
+          Invoke("ReloadScene", SceneDelay);
       }
       
   }
+
+  void ReloadScene()
+  {
+    SceneManager.LoadScene(0);
+  }
+
 }
